13b5875ad1d457fb87fab93cf46e1d89d36600c2
AlexisLeConte <alexis.leconte@outlook.fr>
2019-04-30 11:57:26 +0200

    Update index

--- Server/scripts/index.script.js
+++ Server/scripts/index.script.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     $.ajax({
-        url: "/action_servlet",
-        data: {"action": "get-restaurants-list"},
+        url: "action_servlet",
+        data: {
+            "action": "get-restaurants-list"
+        },
         dataType: "json",
         success: function(data) {
             $("#restaurants-list").empty()
-            restaurants = data["restaurants"]
+            restaurants = data.restaurants
             for(var i=0; i<restaurants.length; i++) {
-                $("#restaurants-list").append("<tr href='restaurant.html?id=" + restaurants[i].id + "'>")
-                $("#restaurants-list").append("<td>" + restaurants[i].name + "</td>")
-                $("#restaurants-list").append("<td>" + restaurants[i].status + "</td>")
-                $("#restaurants-list").append("<td>" + restaurants[i].eta + "</td>")
-                $("#restaurants-list").append("</tr>")
+                let html = "<tr>"
+                         + "<td href='restaurant.html?id=" + restaurants[i].id + "'>" + restaurants[i].name + "</td>"
+                         + "<td>" + restaurants[i].status + "</td>"
+                         + "<td>" + restaurants[i].eta + "</td>"
+                         + "</tr>"
+                $("#restaurants-list").append(html)
             }
         }
     });
 })
-    
+
--- Server/scripts/restaurant.script.js
+++ Server/scripts/restaurant.script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+    
+    
     var restaurant = window.location.href.substr(window.location.href.indexOf('=') + 1)
     $.ajax({
-        url: "/action_servlet",
-        data: {"action": "get-restaurants-details",
-               "id": restaurant},
-        dataType: "json",
+        url: "client",
+        data: {
+            action: "get-restaurants-details",
+            id: restaurant
+        },
         success: function(data) {
             $("#restaurant-name").html(data.name)
             $("#restaurant-theme").html(data.theme)
-
+            $("#restaurant-schedule").html(data.schedule)
+            $("#restaurant-status").html(data.status)
+            $("#restaurant-throughput").html(data.throughput)
+            $("#restaurant-current-waiting-time").html(data.eta)
         }
     })
 })
-    
+
