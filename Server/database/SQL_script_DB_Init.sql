8c1a8529308030c508491356097ef6921a0f7b0e
romaindec <44019433+romaindec@users.noreply.github.com>
2019-05-02 10:55:41 +0200

    added SQL scripts for inserts and test inserts

--- Server/database/SQL_script_DB_Init.sql
+++ Server/database/SQL_script_DB_Init.sql
@@ -90,13 +90,3 @@ CREATE TABLE IF NOT EXISTS `mydb`.`RestaurantAvailabilities` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (, 'BeurkLigneGauche', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (, 'BeurkLigneDroite', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'BeurkLigneCourte', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'PiedDuSaule', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Prevert', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Olivier', NULL);
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Grillon', NULL);
-
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Infrarouge', 'binary');
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Ultrason', 'cm');
---
+++ Server/database/SQL_script_DB_Inserts.sql
@@ -0,0 +1,10 @@
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'BeurkLigneGauche', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'BeurkLigneDroite', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'BeurkLigneCourte', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'PiedDuSaule', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Prevert', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Olivier', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Grillon', NULL);
+
+INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Infrarouge', 'binary');
+INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Ultrason', 'cm');
