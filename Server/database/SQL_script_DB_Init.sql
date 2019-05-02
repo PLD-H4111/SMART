c3b6451a954519f9fb8fcee072539ecdd3527ed3
romaindec <44019433+romaindec@users.noreply.github.com>
2019-05-02 10:31:16 +0200

    updated SQL script for database initialization

--- Server/database/SQL_script_DB_Init.sql
+++ Server/database/SQL_script_DB_Init.sql
@@ -1,39 +1,17 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`Archive` (
-  `PK_idArchive` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tempsAttente` INT UNSIGNED NULL,
-  `FK_restaurant` INT UNSIGNED NOT NULL,
-  `date` DATETIME NOT NULL,
-  `FK_event` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`PK_idArchive`),
-  INDEX `FK_restaurant_idx` (`FK_restaurant` ASC),
-  INDEX `FK_event_idx` (`FK_event` ASC),
-  CONSTRAINT `FK_restaurant`
-    FOREIGN KEY (`FK_restaurant`)
-    REFERENCES `mydb`.`Restaurant` (`PK_idRestaurant`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `FK_event`
-    FOREIGN KEY (`FK_event`)
-    REFERENCES `mydb`.`Event` (`PK_idEvent`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-
 CREATE TABLE IF NOT EXISTS `mydb`.`Event` (
   `PK_idEvent` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `event` VARCHAR(45) NOT NULL,
-  `dateDebut` DATE NULL,
-  `dateFin` DATE NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `beginningDate` DATE NOT NULL,
+  `endingDate` DATE NOT NULL,
   PRIMARY KEY (`PK_idEvent`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Measure` (
   `PK_idMeasure` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `time` TIMESTAMP(10) NOT NULL,
-  `FK_sensor` INT UNSIGNED NOT NULL,
+  `dateTime` DATETIME NOT NULL,
   `measure` DECIMAL(10) NOT NULL,
+  `FK_sensor` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`PK_idMeasure`),
   INDEX `FK_sensor_idx` (`FK_sensor` ASC),
   CONSTRAINT `FK_sensor`
@@ -46,25 +24,20 @@ AUTO_INCREMENT = 1
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Restaurant` (
   `PK_idRestaurant` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `theme` VARCHAR(45) NULL,
   PRIMARY KEY (`PK_idRestaurant`))
-ENGINE = ndbcluster
+ENGINE = InnoDB
 AUTO_INCREMENT = 1
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Sensor` (
   `PK_idSensor` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `FK_sensorType` INT UNSIGNED NOT NULL,
+  `position` INT UNSIGNED NOT NULL,
   `FK_restaurant` INT UNSIGNED NOT NULL,
-  `FK_sensorPosition` INT UNSIGNED NOT NULL,
+  `FK_sensorType` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`PK_idSensor`),
-  INDEX `FK_sensorPosition_idx` (`FK_sensorPosition` ASC),
   INDEX `FK_sensorRestaurant_idx` (`FK_restaurant` ASC),
   INDEX `FK_sensorType_idx` (`FK_sensorType` ASC),
-  CONSTRAINT `FK_sensorPosition`
-    FOREIGN KEY (`FK_sensorPosition`)
-    REFERENCES `mydb`.`SensorPosition` (`PK_idSensorPosition`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE,
   CONSTRAINT `FK_restaurant`
     FOREIGN KEY (`FK_restaurant`)
     REFERENCES `mydb`.`Restaurant` (`PK_idRestaurant`)
@@ -78,45 +51,52 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Sensor` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 
-CREATE TABLE IF NOT EXISTS `mydb`.`SensorPosition` (
-  `PK_idSensorPosition` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `SensorPosition` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`PK_idSensorPosition`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-
 CREATE TABLE IF NOT EXISTS `mydb`.`SensorType` (
   `PK_idSensorType` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(45) NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
   `unit` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`PK_idSensorType`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'rentrée scolaire', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'veille de vacances', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'vacances', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'période de partiels PC', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'effectif réduit', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, 'problème sur le système de détection', NULL, NULL);
-INSERT INTO `mydb`.`Event` (`PK_idEvent`, `event`, `dateDebut`, `dateFin`) VALUES (DEFAULT, '24h INSA', NULL, NULL);
+CREATE TABLE IF NOT EXISTS `mydb`.`WaitingTime` (
+  `PK_idWaitingTime` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `waitingTime` INT UNSIGNED NOT NULL,
+  `date` DATETIME NOT NULL,
+  `FK_restaurant` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`PK_idWaitingTime`),
+  INDEX `FK_restaurant_idx` (`FK_restaurant` ASC),
+  CONSTRAINT `FK_restaurant`
+    FOREIGN KEY (`FK_restaurant`)
+    REFERENCES `mydb`.`Restaurant` (`PK_idRestaurant`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
 
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (, 'BeurkLigneGauche');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (, 'BeurkLigneDroite');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (DEFAULT, 'BeurkLigneCourte');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (DEFAULT, 'PiedDuSaule');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (DEFAULT, 'Prevert');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (DEFAULT, 'Olivier');
-INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `description`) VALUES (DEFAULT, 'Grillon');
+CREATE TABLE IF NOT EXISTS `mydb`.`RestaurantAvailabilities` (
+  `PK_idRestaurantAvailabilities` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NOT NULL,
+  `openingTime` DATETIME NOT NULL,
+  `closingTime` DATETIME NOT NULL,
+  `FK_restaurant` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`PK_idRestaurantAvailabilities`),
+  INDEX `fk_Restaurant_idx` (`FK_restaurant` ASC),
+  CONSTRAINT `fk_Restaurant`
+    FOREIGN KEY (`FK_restaurant`)
+    REFERENCES `mydb`.`Restaurant` (`PK_idRestaurant`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
 
-INSERT INTO `mydb`.`SensorPosition` (`PK_idSensorPosition`, `SensorPosition`) VALUES (DEFAULT, 'position1');
-INSERT INTO `mydb`.`SensorPosition` (`PK_idSensorPosition`, `SensorPosition`) VALUES (DEFAULT, 'position2');
-INSERT INTO `mydb`.`SensorPosition` (`PK_idSensorPosition`, `SensorPosition`) VALUES (DEFAULT, 'position3');
-INSERT INTO `mydb`.`SensorPosition` (`PK_idSensorPosition`, `SensorPosition`) VALUES (DEFAULT, 'position4');
-INSERT INTO `mydb`.`SensorPosition` (`PK_idSensorPosition`, `SensorPosition`) VALUES (DEFAULT, 'position5');
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (, 'BeurkLigneGauche', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (, 'BeurkLigneDroite', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'BeurkLigneCourte', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'PiedDuSaule', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Prevert', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Olivier', NULL);
+INSERT INTO `mydb`.`Restaurant` (`PK_idRestaurant`, `name`, `theme`) VALUES (DEFAULT, 'Grillon', NULL);
 
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `description`, `unit`) VALUES (DEFAULT, 'Infrarouge', 'binary');
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `description`, `unit`) VALUES (DEFAULT, 'Ultrason', 'cm');
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `description`, `unit`) VALUES (DEFAULT, 'Wifi', 'persons');
-INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `description`, `unit`) VALUES (DEFAULT, 'Borne', '?');
-
+INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Infrarouge', 'binary');
+INSERT INTO `mydb`.`SensorType` (`PK_idSensorType`, `type`, `unit`) VALUES (DEFAULT, 'Ultrason', 'cm');
