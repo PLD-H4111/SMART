b6037843638460dedb4a6de1cc998e22caa3d8a8
Mohamed Challal <mohamed.challal@insa-lyon.fr>
2019-05-06 10:46:12 +0200

    connexion between server and database established for the 2 services

--- Server/servlets/action_servlet.py
+++ Server/servlets/action_servlet.py
@@ -3,25 +3,37 @@
 
 
 import json
+import mysql.connector
 
 import service1
 import service2
 
 
-def fetch(input_data):
-	""" """
-	if "action" not in input_data:
-		# pas la bonne structure de l'input data
-		pass
 
-	print("action servlet:", input_data["action"][0])
+
+class ActionServlet:
+	def __init__(self):
+		self.mydb = mysql.connector.connect(
+			host="localhost",
+			user="root",
+			passwd="toorTOOR2019!",
+			database="main"
+		)
 	
-	if input_data["action"][0] == "get-restaurants-list":	
-		return service1.get_restaurants()
-	elif input_data["action"][0] == "get-restaurant-details":
-		return service2.get_restaurant_details(input_data["id"][0], input_data["date"][0])
-	else:
-		pass
+	def fetch(self, input_data):
+		""" """
+		if "action" not in input_data:
+			# pas la bonne structure de l'input data
+			pass
+
+		print("action servlet:", input_data["action"][0])
+		
+		if input_data["action"][0] == "get-restaurants-list":	
+			return service1.get_restaurants(self.mydb)
+		elif input_data["action"][0] == "get-restaurant-details":
+			return service2.get_restaurant_details(self.mydb, input_data["id"][0], input_data["date"][0])
+		else:
+			pass
 	
 	
 
--- Server/servlets/service1.py
+++ Server/servlets/service1.py
@@ -7,19 +7,17 @@ from importlib import reload
 import sys
 sys.path.insert(0, '../database/')
 reload(sys)
-<<<<<<< Updated upstream
 #import bdr
 =======
 import bdr
 """
->>>>>>> Stashed changes
 
 import json
 
 
 
 
-def get_restaurants():
+def get_restaurants(database):
 	""" """
 	restaurants = '''{"restaurants": 
 		[
@@ -31,56 +29,19 @@ def get_restaurants():
 		]
 	}'''
 	
-	import mysql.connector
-
-	mydb = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		passwd="toorTOOR2019!",
-		database="main"
-	)
-
-	print(mydb)
-
 	request = "select * from restaurant;"
-
-	mycursor = mydb.cursor()
-	mycursor.execute(request)
+	mycursor = database.cursor()
 	
+	mycursor.execute(request)
 	myresult = mycursor.fetchall()
 
 	restaurants = """{"restaurants": ["""
 	for i, restaurant in enumerate(myresult):
-		restaurants += """{{ "id":{}, "name":{}, "theme": {} ,"status":"closed", "eta":2500 }}""".format(restaurant[0], restaurant[1], restaurant[2])
-		if i != len(myresult):
+		restaurants += """{{ "id":{}, "name":"{}", "theme": "{}" ,"status":"closed", "eta":2500 }}""".format(restaurant[0], restaurant[1], restaurant[2])
+		if i != len(myresult)-1:
 			restaurants += ","
 	restaurants += """ ] } """
 
 	return restaurants
 	
-	
 
-def test():
-	#Code Exemple
-<<<<<<< Updated upstream
-	#a = bdr.sgbd("../database/mydb.db") # metis
-	#a.execute_script_from_file("../database/SQL_script_DB_Init.sql") # SQL_script_DB_Init
-	#a.commit()
-	#donnee = ("titi", )
-	#results = a.execution("PRAGMA database_list;") #"SELECT * FROM Restaurant")
-	#results = a.execution("attach database 'example.db' as another_db;")
-	#a.print_all_results(results)
-	#a.exit()
-	pass
-=======
-	a = bdr.sgbd("../database/mydb.db") # metis
-	a.execute_script_from_file("../database/SQL_script_DB_Init.sql") # SQL_script_DB_Init
-	a.commit()
-	donnee = ("titi", )
-	results = a.execution("PRAGMA database_list;") #"SELECT * FROM Restaurant")
-	results = a.execution("attach database 'example.db' as another_db;")
-	a.print_all_results(results)
-	a.exit()
->>>>>>> Stashed changes
-
-#test()
