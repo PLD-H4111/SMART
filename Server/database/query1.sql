bcdf52f923c5036b4ec2f1cf109059c8096e05bc
romaindec <44019433+romaindec@users.noreply.github.com>
2019-05-06 22:58:35 +0200

    added SQL query for momoh 1

---
+++ Server/database/query1.sql
@@ -0,0 +1,55 @@
+SELECT PK_idRestaurant, name, theme, "1" AS status, w AS eta
+FROM restaurant, restaurantAvailabilities, waitingTime,
+	(SELECT o.waitingTime as w, name as nom
+
+	FROM restaurant, waitingtime o LEFT JOIN waitingtime b
+
+		ON o.FK_restaurant = b.FK_restaurant AND o.date < b.date
+
+	WHERE PK_idRestaurant = o.FK_restaurant
+
+	AND b.date is NULL
+
+    )o
+
+WHERE PK_idRestaurant = restaurantAvailabilities.FK_restaurant
+
+AND nom = name
+AND PK_idRestaurant = waitingtime.FK_restaurant
+
+AND restaurantAvailabilities.date = date(NOW())
+
+AND openingTime < time(NOW())
+
+AND closingTime > time(NOW())
+UNION
+
+SELECT PK_idRestaurant, name, theme, "0" AS status, w AS eta
+FROM restaurant, 
+
+	(SELECT o.waitingTime as w, name as nom
+
+	FROM restaurant, waitingtime o LEFT JOIN waitingtime b
+
+		ON o.FK_restaurant = b.FK_restaurant AND o.date < b.date
+
+	WHERE PK_idRestaurant = o.FK_restaurant
+
+	AND b.date is NULL
+
+    )o
+
+WHERE nom = name
+
+AND name NOT IN (
+	SELECT name FROM restaurant, restaurantAvailabilities 
+	
+	WHERE PK_idRestaurant = FK_restaurant
+
+	AND restaurantAvailabilities.date = date(NOW())
+	
+	AND openingTime < time(NOW())
+	
+	AND closingTime > time(NOW())
+	)
+;
