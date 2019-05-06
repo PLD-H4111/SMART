b237142db38719f565be5eec5a9f37acd1fbfe9d
AlexisLeConte <alexis.leconte@outlook.fr>
2019-05-06 19:01:46 +0200

    Add login form

---
+++ Server/scripts/login.js
@@ -0,0 +1,26 @@
+function login() {
+    let login = $("#input-login").val();
+    let password = $("#input-password").val();
+    
+    $.ajax({
+        url: "/action_servlet",
+        data: {
+            action: "admin-login",
+            login: login,
+            password: password
+        },
+        success: function(data) {
+            if(data.success == 1) {
+                window.location.href = "admin.html";
+            } else {
+                $("#feedback").html(
+                    '<div class="panel panel-danger">'
+                  + '<div class="panel-heading">Erreur</div>'
+                  + '<div class="panel-body">Identifiants de connexion invalides</div>'
+                  + '</div>"'
+                );
+            }
+        }
+    });
+}
+
